7f57298b4f32623d1dd002678f6ecc0c32e7d154
Bala_Tringer <91451601+bala-tringapps@users.noreply.github.com>
2021-10-26 15:41:55 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,14 +6,16 @@ import axios from 'axios';
 function App() {
 
   const [fruit, setFruit] = useState([]);
-  const [identity, setId] = useState(0);
+  
   const [search, setSearch] = useState("");
-  const [name, setName] = useState(0);
+  
   const [filteritems, setFilteritems] = useState([]);
   
  
 
   useEffect(() => {
+    const [identity, setId] = useState(0);
+    const [name, setName] = useState(0);
     axios.get('http://localhost:3010/testApi')
     .then(res => {
       setFruit(res.data);
